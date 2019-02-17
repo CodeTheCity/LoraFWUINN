3d566da748baa4b6a9d201ceb32eed593c78fdd3
ilmikko <mikko@5x.fi>
2019-02-17 15:19:31 +0000

    Parse some JSON for show effect.

--- aggregation_api/api.rb
+++ aggregation_api/api.rb
@@ -4,11 +4,19 @@ $DIR = File.dirname(__FILE__);
 $LOAD_PATH << "#{$DIR}/lib";
 
 require 'server';
+require 'json';
+require 'base64';
 
 $host = 'localhost';
 $port = 9000;
 
 server = Server.new($host, $port){ |context|
-  puts "Request from: #{context.ip} body: #{context.body}";
+  puts "Request from: #{context.ip}";
+  json = JSON.parse(context.body);
+
+  puts "App: #{json['app_id']}";
+  puts "Dev: #{json['dev_id']}";
+  puts "Payload: #{Base64.decode64(json['payload_raw'])}";
+  
   context.send("Hello world");
 };
