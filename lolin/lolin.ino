0d795324b7ccd3448fe301ed2311dcbfabfe5c7e
ilmikko <mikko@5x.fi>
2019-02-17 18:33:43 +0000

    Use the EOT character to mark the serial end of transmission.

--- lolin/lolin.ino
+++ lolin/lolin.ino
@@ -3,9 +3,14 @@ void setup() {
   pinMode(2, OUTPUT);
 }
 
+void sendMessageToLoRa(const char string[]) {
+  Serial.write(string);
+  Serial.write("\004");
+}
+
 void loop() {
   digitalWrite(2, HIGH);
-  Serial.println("This is a top secret message");
+  sendMessageToLoRa("This is a top secret message");
   delay(1000);
   digitalWrite(2, LOW);
   delay(1000);
